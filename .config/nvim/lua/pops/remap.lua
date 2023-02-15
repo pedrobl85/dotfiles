1da734f82d43bce5d56c4b7b812d71ec26d86595
pedrobl85 <pedrobl1718@gmail.com>
2023-02-14 23:08:30 -0300

    Add keybind <C-f> in vim for tmux-sessionizer

--- .config/nvim/lua/pops/remap.lua
+++ .config/nvim/lua/pops/remap.lua
@@ -20,4 +20,4 @@ vim.keymap.set("n", "<leader>Y", "\"+Y")
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
 
--- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
